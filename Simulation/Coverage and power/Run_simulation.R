85c03ccb574494bc00931884a32ba2e566eea53f
Kevin Wilk (lokal) <wilk@PC12024.Mathematik.Uni-Marburg.DE>
2026-03-31 21:52:02 +0200

    R script with a ‘for’ loop to run ‘Coverage_and_power_of_Test.R’

---
+++ Simulation/Coverage and power/Run_simulation.R
@@ -0,0 +1,27 @@
+library(MASS)
+library(ggplot2)
+library(reshape2)
+library(locpol)
+library(interp)
+library(stats)
+library(future.apply)
+library(parallel)
+library(biLocPol)
+library(plotly)
+library(tidyr)
+library(goffda)
+library(crayon)
+library(future)
+library(hms)
+library(dplyr)
+library(tibble)
+library(gridExtra)
+
+
+for(j in 2:10){
+
+    i = 2
+
+    source("Power and Coverage of Test.R")
+    
+}
