15bfa1ba98c73ab3360c2f7109050f16a1d83cee
Kevin Wilk (lokal) <wilk@PC12024.Mathematik.Uni-Marburg.DE>
2026-04-08 11:18:37 +0200

    Calculating long run kernel

--- weather temperature/long run kernel/lagged_covariance.R
+++ weather temperature/long run kernel/lagged_covariance.R
@@ -7,7 +7,7 @@ library(stats)
 library(future)
 library(future.apply)
 library(parallel)
-library(tidyverse)
+library(tidyr)
 library(lubridate)
 library(hms)
 library(dplyr)
@@ -20,7 +20,8 @@ library(tensorA)
 
 
 source("Mod_biLocPol.R")
-source("weather temperature/R files/functions.R")
+source("weather_temperature/functions.R")
+source("weather_temperature/long_run_kernel/function_test.R")
 
 ############################################################################
 k = 1  # Change to: 1 (Berlin)                                            ##
@@ -28,7 +29,7 @@ k = 1  # Change to: 1 (Berlin)                                            ##
 #                   3 (Hamburg)                                           ##
 #                   4 (Munich)                                            ##
 data.example = list("Berlin", "Frankfurt_Main", "Hamburg", "Munich")      ##
-load(paste0("weather temperature/data sets/",data.example[[k]],".RData")) ##
+load(paste0("weather_temperature/data_sets/",data.example[[k]],".RData")) ##
 ############################################################################
 
 
@@ -44,11 +45,11 @@ cov.d.list = list()
 
 # Optional: For parallelizing computations ####################
 options(future.globals.maxSize = 64 * 1024^3)                ##   
-plan(multisession, workers = 120) # MaRC3a: partition=mqtest ##
+plan(multisession, workers = 20) # MaRC3a: partition=mqtest ##
 #plan(multisession, workers = 60) # MaRC3a: partition=normal ##
 ###############################################################
 
-bandwidth.d = readRDS(paste0("weather temperature/bandwidth selection/Results/bw_Gamma_d_",data.example[[k]],".rds"))
+bandwidth.d = readRDS(paste0("weather_temperature/bandwidth_selection/Results/bw_Gamma_d_",data.example[[k]],".rds"))
 
 for(m in 1:12){
   
@@ -69,9 +70,9 @@ for(m in 1:12){
     lag.k.Gamma   = list()
     lag.k.Gamma.d = list()  
   
-    file.s = paste0("weather temperature/kernel weights/full_w_s_lag0.rds")
-    file.d = paste0("weather temperature/kernel weights/full_w_d_lag0_",sprintf("%03d", as.integer(bandwidth.d[m] * 100)),".rds") #bandwidth.d needs to be imported!!!!!!!!!!
-  
+    file.s = paste0("weather_temperature/kernel_weights/full_w_s_lag0.rds")
+    if(bandwidth.d[m] < 0.1){ file.d = paste0("weather_temperature/kernel_weights/full_w_d_lag0_",sprintf("%03d", as.integer(bandwidth.d[m] * 100)),".rds")
+    }else{                    file.d = paste0("weather_temperature/kernel_weights/full_w_d_lag0_",sprintf("%02d", as.integer(bandwidth.d[m] * 10)),".rds")}
     w   = readRDS(file.s)
     wd  = readRDS(file.d)
   
@@ -85,7 +86,7 @@ for(m in 1:12){
     rm(wd,lag.Gamma.d)
   
     lag.k.Gamma.part2 = future_lapply(1:12, function(k) {
-      file.s = paste0("weather temperature/kernel weights/full_w_s_lag",k,".rds")
+      file.s = paste0("weather_temperature/kernel_weights/full_w_s_lag",k,".rds")
       w.lag = readRDS(file.s)
       n.year = unique(sample.sparse$Year) 
       lag.Gamma = Reduce(`+`,lapply(n.year,  function(j){eval.weights(w.lag, observation.transformation(sample.sparse[which(sample.sparse$Year %in% j), -1], lag = k, grid.type = "lesseq", periodic = T, m = 24), lag = k)}))/length(n.year)
@@ -94,7 +95,8 @@ for(m in 1:12){
     lag.k.Gamma = c(lag.k.Gamma,lag.k.Gamma.part2)
   
     lag.k.Gamma.d.part2 = future_lapply(1:12, function(k) {  
-      file.d = paste0("weather temperature/kernel weights/full_w_d_lag",k,"_",sprintf("%03d", as.integer(bandwidth.d[m] * 100)),".rds")
+      if(bandwidth.d[m] < 0.1){ file.d = paste0("weather_temperature/kernel_weights/full_w_d_lag",k,"_",sprintf("%03d", as.integer(bandwidth.d[m] * 100)),".rds")}
+      else{                     file.d = paste0("weather_temperature/kernel_weights/full_w_d_lag",k,"_",sprintf("%02d", as.integer(bandwidth.d[m] * 10)),".rds")}
       w.lag = readRDS(file.d)
       n.year = unique(sample.dense$Year) 
       lag.Gamma = Reduce(`+`,lapply(n.year,  function(j){eval.weights(w.lag, observation.transformation(sample.dense[which(sample.dense$Year %in% j), -1], lag = k, grid.type = "lesseq", periodic = T, m = 144), lag = k)}))/length(n.year)
@@ -112,8 +114,17 @@ for(m in 1:12){
     print(paste0("done: ", month.name[m]))
 }
 
-plan(sequential)
 
+saveRDS(cov.d.list , paste0("weather_temperature/long_run_kernel/Results/list_Gamma_d_",data.example[[k]],".rds"))
+saveRDS(cov.s.list , paste0("weather_temperature/long_run_kernel/Results/list_Gamma_s_",data.example[[k]],".rds"))
+
+
+
+#############################################################################################################################
+# Skip: Estimating lag covariance kernels for lag = 0,...,12 ################################################################
+# cov.s.list = readRDS(paste0("weather temperature/long run kernel/Results/list_Gamma_s_",data.example[[k]],".rds")) ########
+# cov.d.list = readRDS(paste0("weather temperature/long run kernel/Results/list_Gamma_d_",data.example[[k]],".rds")) ########
+#############################################################################################################################
 
 
 
@@ -124,6 +135,7 @@ plan(sequential)
 #######################
 # Finding maximal lag #
 #######################
+
 
 start.24 = 21; end.24 = 117 
 
@@ -142,49 +154,55 @@ rownames(rho.hat.d.df) = paste0("lag ", 1:12)
 rho.hat.d.df = rho.hat.d.df |> rownames_to_column("lag") |> mutate(lag = as.integer(gsub("lag\\s*", "", lag))) |> pivot_longer(cols = -lag,names_to = "MONTH",values_to = "value") |> mutate(MONTH = factor(MONTH, levels = month.name))
 
 
- 
-# Optional: For parallelizing computations ####################
-options(future.globals.maxSize = 64 * 1024^3)                ##   
-plan(multisession, workers = 120) # MaRC3a: partition=mqtest ##
-#plan(multisession, workers = 60) # MaRC3a: partition=normal ##
-###############################################################
-
-
 test.s.val = lapply(1:12, function(month) {inference.autocovariance.test(data.s.24h   |> select(1:27) , m = month, alpha = 0.95, max.lag = 12)})
-test.d.val = lapply(1:12, function(month) {inference.autocovariance.test(dense.hourly |> select(1:27) , m = month, alpha = 0.95, max.lag = 12)}) 
-
-plan(sequential)
-
+test.d.val = lapply(1:12, function(month) {inference.autocovariance.test(data.d.24h.hourly |> select(1:27) , m = month, alpha = 0.95, max.lag = 12)}) 
 
 test.s.val = data.frame(lag = rep(c(0.6,2:11,12.5), times = 12), value = unlist(test.s.val), MONTH = factor(rep(month.name[1:length(test.s.val)], lengths(test.s.val)), levels = month.name))
 test.d.val = data.frame(lag = rep(c(0.6,2:11,12.5), times = 12), value = unlist(test.d.val), MONTH = factor(rep(month.name[1:length(test.d.val)], lengths(test.d.val)), levels = month.name))
 
+plan(sequential)
+
+
+#############################################################################################################################
+# Skip running test.s.val and test.d.val                     ################################################################
+# test.s.val = readRDS(paste0("weather_temperature/long_run_kernel/Results/test_max_lag_s_",data.example[[k]],".rds")) ######
+# test.d.val = readRDS(paste0("weather_temperature/long_run_kernel/Results/test_max_lag_d_",data.example[[k]],".rds")) ######
+#############################################################################################################################
+
+
+
+
+
 ggplot() +
-  geom_bar(aes(x = lag,  y     = value), stat = "identity", fill = "turquoise",width = 0.4, data = rho.hat.d.df, alpha = 0.7) +
-  geom_line(aes(x = lag, y = value), test.d.val, col = "green3", size = 1.1, lty = 2)+
+  geom_bar( aes(x = lag,     y = value), stat = "identity", fill = "turquoise",width = 0.4, data = rho.hat.d.df, alpha = 0.7) +
+  geom_line(aes(x = lag,     y = value), test.d.val, col = "green3", size = 1.1, lty = 2)+
   geom_line(aes(x = lag+0.3, y = value), test.s.val, col = "blue", size = 1.1, lty = 2)+
-  geom_bar(aes(x = lag+0.3, y = value),  stat = "identity", fill = "darkblue", width = 0.4, data = rho.hat.s.df, alpha = 0.5) +
+  geom_bar( aes(x = lag+0.3, y = value),  stat = "identity", fill = "darkblue", width = 0.4, data = rho.hat.s.df, alpha = 0.5) +
   labs(x = "Lag", y = bquote(hat(rho)[Lag]) ,title = "") +
-  theme(plot.title = element_text(size =15),
-        legend.text = element_text(size =15),
-        strip.text = element_text(size = 15),
+  
+  theme(plot.title   = element_text(size = 15),
+        legend.text  = element_text(size = 15),
+        strip.text   = element_text(size = 15),
         legend.title = element_text(size = 15),
         axis.title.x = element_text(size = 15),     
         axis.title.y = element_text(size = 15),
         axis.text.x  = element_text(size = 15),     
-        axis.text.y  = element_text(size = 15))+
-  scale_y_continuous(breaks = c(0,0.25,0.5), limits = c(0,0.7))+
-  scale_x_continuous(breaks = c(1,4,8,12), limits = c(0.5,12.8))+
+        axis.text.y  = element_text(size = 15)) +
+  
+  scale_y_continuous(breaks = c(0,0.25,0.5), limits = c(0,0.7)) +
+  scale_x_continuous(breaks = c(1,4,8,12), limits = c(0.5,12.8)) +
   facet_wrap(~ MONTH)
+
 ggsave(paste0("Application/Data examples/",data.example[[l]],"/pictures/lag test.png"), width = 32, height = 18, units = "cm", dpi = 300)
 
-month.max.lag
 
-month.d.max.lag = c(2, 2, 1, 1, 1, 2, 2, 1, 2, 1, 2, 1);month.s.max.lag = c(2, 1, 2, 2, 3, 4, 2, 2, 2, 2, 1, 1)#Berlin
-month.d.max.lag = c(2, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2);month.s.max.lag = c(2, 0, 2, 4, 2, 4, 4, 2, 0, 1, 1, 1)#Frankfurt
-month.d.max.lag = c(2, 2, 1, 1, 1, 2, 1, 2, 2, 1, 1, 1);month.s.max.lag = c(2, 1, 1, 2, 1, 4, 1, 2, 0, 1, 1, 1)#Hamburg
-month.d.max.lag = c(2, 2, 1, 1, 1, 2, 1, 1, 2, 1, 2, 1);month.s.max.lag = c(2, 1, 2, 2, 3, 5, 2, 2, 1, 1, 1, 1)#Leipzig
-month.d.max.lag = c(2, 2, 1, 1, 1, 2, 2, 1, 2, 1, 1, 2);month.s.max.lag = c(3, 1, 2, 3, 3, 5, 2, 2, 1, 2, 1, 2)#Muenchen
+
+
+month.d.max.lag = c(2, 2, 1, 1, 1, 2, 2, 1, 2, 1, 2, 1);month.s.max.lag = c(2, 1, 2, 2, 3, 4, 2, 2, 2, 2, 1, 1)# Berlin
+month.d.max.lag = c(2, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2);month.s.max.lag = c(2, 0, 2, 4, 2, 4, 4, 2, 0, 1, 1, 1)# Frankfurt
+month.d.max.lag = c(2, 2, 1, 1, 1, 2, 1, 2, 2, 1, 1, 1);month.s.max.lag = c(2, 1, 1, 2, 1, 4, 1, 2, 0, 1, 1, 1)# Hamburg
+month.d.max.lag = c(2, 2, 1, 1, 1, 2, 1, 1, 2, 1, 2, 1);month.s.max.lag = c(2, 1, 2, 2, 3, 5, 2, 2, 1, 1, 1, 1)# Leipzig
+month.d.max.lag = c(2, 2, 1, 1, 1, 2, 2, 1, 2, 1, 1, 2);month.s.max.lag = c(3, 1, 2, 3, 3, 5, 2, 2, 1, 2, 1, 2)# Muenchen
 month.max.lag = apply(rbind(month.d.max.lag,month.s.max.lag),2,max) 
 
 
@@ -220,14 +238,16 @@ ggplot() +
   labs(x = "Time", y = "Temperature in °C",title = bquote("")) +
   scale_color_manual(values = month.colors, name = "Month") +
   scale_linetype_manual(values = month.lty,limits = month.abb, name = "Month") +
-  theme(plot.title = element_text(size =25),
-        legend.text = element_text(size =29),
-        strip.text = element_text(size = 25),
+  
+  theme(plot.title   = element_text(size = 25),
+        legend.text  = element_text(size = 29),
+        strip.text   = element_text(size = 25),
         legend.title = element_text(size = 30),
         axis.title.x = element_text(size = 25),     
         axis.title.y = element_text(size = 25),
         axis.text.x  = element_text(size = 25),     
-        axis.text.y  = element_text(size = 25))+
+        axis.text.y  = element_text(size = 25)) +
+  
   scale_y_continuous(breaks = c(0, 2.5, 5 , 7.5, 10), limits = c(0, 9.5)) +
   scale_x_time(breaks = as_hms(c("00:00:00", "10:00:00", "20:00:00")),labels = c("00:00", "10:00", "20:00"))
 ggsave(paste0("Application/Data examples/",data.example[[l]],"/pictures/sd sparse monthly.png"), width = 28, height = 18, units = "cm", dpi = 300)
@@ -237,18 +257,20 @@ ggplot() +
   labs(x = "Time", y = "Temperature in °C",title = bquote("")) +
   scale_color_manual(values = month.colors, name = "Month") +
   scale_linetype_manual(values = month.lty,limits = month.abb, name = "Month") +
-  guides(colour   = guide_legend(override.aes = list(colour = NA, linetype = 0))) +
-  theme(plot.title = element_text(size =25),
-        legend.text = element_text(size =29,color = "transparent"),
-        strip.text = element_text(size = 25,  color = "transparent"),
-        legend.title = element_text(size = 30,color = "transparent"),
+  guides(colour      = guide_legend(override.aes = list(colour = NA, linetype = 0))) +
+  
+  theme(plot.title   = element_text(size =25),
+        legend.text  = element_text(size =29,  color = "transparent"),
+        strip.text   = element_text(size = 25, color = "transparent"),
+        legend.title = element_text(size = 30, color = "transparent"),
         axis.title.x = element_text(size = 25),     
         axis.title.y = element_text(size = 25),
         axis.text.x  = element_text(size = 25),     
         axis.text.y  = element_text(size = 25),
         legend.position = "right",
         legend.key = element_rect(fill = NA, colour = NA),
-        legend.background = element_rect(fill = "transparent", color = NA))+
+        legend.background = element_rect(fill = "transparent", color = NA)) +
+  
   scale_y_continuous(breaks = c(0, 2.5, 5 , 7.5, 10), limits = c(0, 9.5)) +
   scale_x_time(breaks = as_hms(c("00:00:00", "10:00:00", "20:00:00")),labels = c("00:00", "10:00", "20:00"))
 ggsave(paste0("Application/Data examples/",data.example[[l]],"/pictures/sd dense monthly.png"), width = 28, height = 18, units = "cm", dpi = 300)
@@ -259,14 +281,16 @@ ggplot() +
   labs(x = "Time", y = "Temperature in °C",title = bquote("")) +
   scale_color_manual(values = month.colors, name = "Month") +
   scale_linetype_manual(values = month.lty,limits = month.abb, name = "Month") +
-  theme(plot.title = element_text(size =25),
-        legend.text = element_text(size =29),
-        strip.text = element_text(size = 25),
+  
+  theme(plot.title   = element_text(size =25),
+        legend.text  = element_text(size =29),
+        strip.text   = element_text(size = 25),
         legend.title = element_text(size = 30),
         axis.title.x = element_text(size = 25),     
         axis.title.y = element_text(size = 25),
         axis.text.x  = element_text(size = 25),     
-        axis.text.y  = element_text(size = 25))+
+        axis.text.y  = element_text(size = 25)) +
+  
   scale_y_continuous(breaks = c(0, 2.5, 5 , 7.5, 10), limits = c(0, 9.5)) +
   scale_x_time(breaks = as_hms(c("00:00:00", "10:00:00", "20:00:00")),labels = c("00:00", "10:00", "20:00"))
 ggsave(paste0("Application/Data examples/",data.example[[l]],"/pictures/sd sparse monthly long run.png"), width = 28, height = 18, units = "cm", dpi = 300)
@@ -276,18 +300,20 @@ ggplot() +
   labs(x = "Time", y = "Temperature in °C",title = bquote("")) +
   scale_color_manual(values = month.colors, name = "Month") +
   scale_linetype_manual(values = month.lty,limits = month.abb, name = "Month") +
-  guides(colour   = guide_legend(override.aes = list(colour = NA, linetype = 0))) +
-  theme(plot.title = element_text(size =25),
-        legend.text = element_text(size =29,color = "transparent"),
-        strip.text = element_text(size = 25,  color = "transparent"),
-        legend.title = element_text(size = 30,color = "transparent"),
+  guides(colour      = guide_legend(override.aes = list(colour = NA, linetype = 0))) +
+  
+  theme(plot.title   = element_text(size = 25),
+        legend.text  = element_text(size = 29, color = "transparent"),
+        strip.text   = element_text(size = 25, color = "transparent"),
+        legend.title = element_text(size = 30, color = "transparent"),
         axis.title.x = element_text(size = 25),     
         axis.title.y = element_text(size = 25),
         axis.text.x  = element_text(size = 25),     
         axis.text.y  = element_text(size = 25),
         legend.position = "right",
         legend.key = element_rect(fill = NA, colour = NA),
-        legend.background = element_rect(fill = "transparent", color = NA))+
+        legend.background = element_rect(fill = "transparent", color = NA)) +
+  
   scale_y_continuous(breaks = c(0, 2.5, 5 , 7.5, 10), limits = c(0, 9.5)) +
   scale_x_time(breaks = as_hms(c("00:00:00", "10:00:00", "20:00:00")),labels = c("00:00", "10:00", "20:00"))
 ggsave(paste0("Application/Data examples/",data.example[[l]],"/pictures/sd dense monthly long run.png"), width = 28, height = 18, units = "cm", dpi = 300)
