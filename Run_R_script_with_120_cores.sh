15afc2915ed344be38f3bdbaee698c517e29c31f
Kevin Wilk (lokal) <wilk@PC12024.Mathematik.Uni-Marburg.DE>
2026-04-15 13:15:33 +0200

    shell script for MaRC3a with 120 cores (parallelization)

--- Run_R_script_with_120_cores.sh
+++ Run_R_script_with_120_cores.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=Simulation_future
 #SBATCH --partition=mqtest
-#SBATCH --time=9-23:59:00
+#SBATCH --time=2-23:59:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=120
@@ -16,13 +16,14 @@ export MKL_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 
 cd "$SLURM_SUBMIT_DIR"
-Rscript "weather temperature/confidence bands/constucting_confidence_bands.R" 
+Rscript "Simulation/Coverage and power/Run_simulation.R" 
+
+#Rscript "weather temperature/confidence bands/constucting_confidence_bands.R" 
 #Rscript "weather temperature/bandwidth selection/bandwidth_selection.R" 
 #Rscript "weather temperature/kernel weights/calculation_weights.R" 
 #Rscript "weather temperature/long run kernel/lagged_covariance.R" 
 
-#Rscript "Simulation/Coverage and power/Run_simulation.R" 
 #Rscript "Simulation/Coverage and power/Coverage_and_power_of_Test.R"
 #Rscript "Simulation/optimal bandwidth/LagCov_bandwidth_selection.R" 
 #Rscript "Simulation/optimal bandwidth/Mean_bandwidth_selection.R" 
-#Rscript "Simulation/weights/Calculating _weights.R" 
+#Rscript "Simulation/weights/Calculating _weights.R"
