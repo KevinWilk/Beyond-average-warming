77649fedb57b0a36379136991618968dd89a0eb2
Kevin Wilk (lokal) <wilk@PC12024.Mathematik.Uni-Marburg.DE>
2026-04-20 13:46:58 +0200

    validate coverage and power of test

--- Simulation/Coverage and power/Coverage_and_power_of_Test.R
+++ Simulation/Coverage and power/Coverage_and_power_of_Test.R
@@ -206,7 +206,7 @@ for(rep in 1:Repitition){
   # Dependent Multiplier Bootstrap #
   ##################################
   
-  dep.sim = q.MB(Y.s, Y.d, estimation$sparse, estimation$dense, kernel.diag, (1:p.eval)/p.eval, bandwidth[c(2,i*2+2),j], B = 10, depend = T, int = integral, H0 = 0) 
+  dep.sim = q.MB(Y.s, Y.d, estimation$sparse, estimation$dense, kernel.diag, (1:p.eval)/p.eval, bandwidth[c(2,i*2+2),j], B = 10, depend = T, int = integral) 
 
   
   dep.q90 = quantile(dep.sim$sample, probs = 0.9,  Type = 2, na.rm = T)
@@ -235,7 +235,7 @@ for(rep in 1:Repitition){
   # Independent Multiplier Bootstrap #
   ####################################
   
-  #ind.sim = q.MB(Y.s, Y.d, estimation$sparse, estimation$dense, kernel.diag, (1:p.eval)/p.eval, bandwidth[c(2,i*2+2),j], B = B, depend = F, int = integral, H0 = 0)
+  #ind.sim = q.MB(Y.s, Y.d, estimation$sparse, estimation$dense, kernel.diag, (1:p.eval)/p.eval, bandwidth[c(2,i*2+2),j], B = B, depend = F, int = integral)
   
   
   #ind.q90 = quantile(ind.sim$sample, probs = 0.9,  Type = 2, na.rm = T)
